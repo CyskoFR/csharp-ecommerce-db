7706b7618f7866106d5e8ea70cfe9dd9fdf75c5e
CyskoFR <franc.sko91@gmail.com>
2022-09-30 17:42:12 +0200

    add CRUD sample

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
 
-Console.WriteLine("mannaggia");
+using (EcommerceContext db = new EcommerceContext())
+{
+    // Create
+
+    Product newProduct = new Product { Name = "Panino", Description = "Un panino ripieno di prosciutto", Price = 4.99 };
+    db.Add(newProduct);
+
+    Customer newCustomer = new Customer { Name = "Mara", Surname = "Maionchi", Email = "maramare@mail.it" };
+    db.Add(newCustomer);
+
+    db.SaveChanges();
+    // Read
+    Console.WriteLine("Recupero lista di Products");
+    List<Product> products = db.Products.OrderBy(product => product.Name).ToList<Product>();
+    foreach (Product product in products)
+    {
+        Console.WriteLine(product.Name + " " + product.Description + " ------> " + product.Price);
+    }
+
+    Console.WriteLine();
+
+    Console.WriteLine("Recupero lista di Customers");
+    List<Customer> customers = db.Customers.OrderBy(customer => customer.Name).ToList<Customer>();
+    foreach (Customer customer in customers)
+    {
+        Console.WriteLine(customer.Name + " " + customer.Surname + " ------> " + customer.Email);
+    }
+
+    //// Update
+    //newCustomer.Name = "Veronica";
+    //db.SaveChanges();
+
+    //// Delete
+    //db.Remove(customer);
+    //db.SaveChanges();
+}
 
 public class EcommerceContext : DbContext
 {
