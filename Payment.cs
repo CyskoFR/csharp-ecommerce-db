4a50c8ad0188a0007f72aff8d08f31d64071b005
CyskoFR <franc.sko91@gmail.com>
2022-09-30 15:52:53 +0200

    add classes

---
+++ Payment.cs
@@ -0,0 +1,10 @@
+﻿public class Payment
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int Amount { get; set; }
+    public bool Status { get; set; }
+
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("mannaggia");
+
+public class EcommerceContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-ecommerce;Integrated Security=True;Pooling=False");
+    }
+}
